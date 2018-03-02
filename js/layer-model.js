271c2d0f46a506dd232dc4075b8c39ac9d824133
NightHawk-Michael <michael.le611@gmail.com>
2018-03-02 17:02:55 +0800

    modify geojson files

--- js/layer-model.js
+++ js/layer-model.js
@@ -1,58 +1,67 @@
 var LayerModel = {
 	layerData: {
 		waterfeature: {
-			url: null,
+			url: "https://raw.githubusercontent.com/michaelhoanglong/GIS-Visualization/master/geojson/water-features.geojson",
 			data: null,
-			color: null,
-			opacity: null		
+			color: "#bd0026",
+			opacity: 0.8,
+			type: "symbol"		
 		},
 		walls: {
-			url: null,
+			url: "https://raw.githubusercontent.com/michaelhoanglong/GIS-Visualization/master/geojson/walls.geojson",
 			data: null,
-			color: null,
-			opacity: null		
+			color: "#f03b20",
+			linewidth: 5,
+			type: "line"		
 		},
 		streams: {
-			url: null,
+			url: "https://raw.githubusercontent.com/michaelhoanglong/GIS-Visualization/master/geojson/streams.geojson",
 			data: null,
-			color: null,
-			opacity: null		
+			color: "#fd8d3c",
+			linewidth: 5,
+			type: "line"		
 		},
 		scenicarea: {
-			url: null,
+			url: "https://raw.githubusercontent.com/michaelhoanglong/GIS-Visualization/master/geojson/scenic-area.geojson",
 			data: null,
-			color: null,
-			opacity: null		
+			color: "#feb24c",
+			opacity: 0.8,
+			type: "symbol"		
 		},
 		rockeries: {
-			url: null,
+			url: "https://raw.githubusercontent.com/michaelhoanglong/GIS-Visualization/master/geojson/rockeries.geojson",
 			data: null,
-			color: null,
-			opacity: null		
+			color: "#253494",
+			opacity: 0.8,
+			type: "symbol"		
 		},
 		perimeterwalls: {
-			url: null,
+			url: "https://raw.githubusercontent.com/michaelhoanglong/GIS-Visualization/master/geojson/perimeter-wall.geojson",
 			data: null,
-			color: null,
-			opacity: null		
+			color: "#fed976",
+			linewidth: 5,
+			type: "line"		
 		},	
 		lakes: {
 			url: "https://raw.githubusercontent.com/michaelhoanglong/GIS-Visualization/master/geojson/lakes.geojson",
 			data: null,
 			color: "#088",
-			opacity: 0.8		
+			opacity: 0.8,
+			type: "fill"		
 		},
 		islands: {
 			url: "https://raw.githubusercontent.com/michaelhoanglong/GIS-Visualization/master/geojson/Islands.geojson",
 			data: null,
 			color: "#8B008B",
-			opacity: 0.8		
+			opacity: 0.8,
+			type: "fill"		
 		},
 		buildings: {
 			url: "https://raw.githubusercontent.com/michaelhoanglong/GIS-Visualization/master/geojson/building.geojson",
 			data: null,
 			color: "#FFFF00",
-			opacity: 0.8		
+			opacity: 0.8,
+			type: "fill"		
 		},
 	},
 	loadData: function(){
@@ -60,25 +69,54 @@ var LayerModel = {
 		for(var layerName in layerData){
 			if(layerData[layerName] != null){
 				if(layerData[layerName]["url"] != null && layerData[layerName]["data"] == null){
+					var layerTemplate = {
+								'id': layerName,
+								'type': layerData[layerName]["type"],
+								'source': {
+									'type': 'geojson',
+									'data': result, 
+								}
+							};
+					if(layerData[layerName]["type"] == "fill"){
+						layerTemplate['paint'] = {
+							'fill-color': layerData[layerName]["color"],
+							'fill-opacity': layerData[layerName]["opacity"]
+						};
+						if(layerName == "buildings"){
+							layerTemplate['layout'] = {
+								"text-field": "{Name}",
+					            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+					            "text-offset": [0, 0.6],
+					            "text-anchor": "top"
+							};
+						}
+					}
+					else if(layerData[layerName]["type"] == "line"){
+						layerTemplate['layout'] = {
+							'line-join': 'round',
+							'line-cap': 'round'
+						};
+						layerTemplate['paint'] = {
+							'line-color': layerData[layerName]["color"],
+							'line-width': layerData[layerName]["linewidth"]
+						};
+					}
+					else{
+						layerTemplate['layout'] = {
+							"icon-image": "circle-15",
+				            "text-field": "{Name}",
+				            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+				            "text-offset": [0, 0.6],
+				            "text-anchor": "top"
+						};
+					}
 					$.ajax({
 						type: "GET",
 						url: layerData[layerName]["url"],
 						async: false,
 						dataType: "json",
 						success: function(result){
-							layerData[layerName]["data"] = {
-								'id': layerName,
-								'type': 'fill',
-								'source': {
-									'type': 'geojson',
-									'data': result, 
-									},
-								'layout': {},
-								'paint': {
-									'fill-color': layerData[layerName]["color"],
-									'fill-opacity': layerData[layerName]["opacity"]
-								}
-							};
+							layerData[layerName]["data"] = layerTemplate;
 						}
 					});
 				}
